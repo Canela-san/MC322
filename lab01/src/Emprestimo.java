a93307ae913711afe2c5ee2fd52c8899c06257f3
Canela-san <sstarksan@gmail.com>
2023-08-08 17:44:51 -0300

    lab01

--- lab01/src/Emprestimo.java
+++ lab01/src/Emprestimo.java
@@ -1,5 +1,6 @@
 import java.util.Date;
 
+
 public class Emprestimo {
     Livro livro;
     Date dataEmprestimo = new Date();
@@ -9,37 +10,50 @@ public class Emprestimo {
     
     public Emprestimo(Livro livro) {
         this.livro = livro;
+        //pegar outras variaveis pela data do sistema
     }
 
     public Livro getLivro() {
         return livro;
     }
+
     public void setLivro(Livro livro) {
         this.livro = livro;
     }
+
     public Date getDataEmprestimo() {
         return dataEmprestimo;
     }
+
     public void setDataEmprestimo(Date dataEmprestimo) {
         this.dataEmprestimo = dataEmprestimo;
     }
+
     public Date getDataDevolucao() {
         return dataDevolucao;
     }
+
     public void setDataDevolucao(Date dataDevolucao) {
         this.dataDevolucao = dataDevolucao;
     }
+
     public int getRenovacoes() {
         return Renovacoes;
     }
+
     public void setRenovacoes(int renovacoes) {
         Renovacoes = renovacoes;
     }
+
     public String getStatus() {
         return status;
     }
+
     public void setStatus(String status) {
         this.status = status;
     }
+
+    
+
     
 }
