5c4fced219a7c6b2c2e9d16ab4655dcfc8baddf2
Canela-san <sstarksan@gmail.com>
2023-08-07 22:07:30 -0300

    teste

---
+++ lab01/lab01.java
@@ -0,0 +1,19 @@
+public class Livro{
+	private String nome;
+	private String edicao;
+	private String categoria;
+	private String descricao;
+
+	public getNome(){
+	return this.nome;
+	}
+
+	public setNome(String nome){
+	this.nome = nome;
+
+	}
+
+}
+public int main(){
+	Livro livro1= new Livro();
+}
---
+++ lab01/lab01/src/Livro.java
@@ -0,0 +1,11 @@
+public class Livro {
+    private String nome;
+    private String edicao;
+    private String categoria;
+    private String descricao;
+    private int quantidade;
+    
+
+
+
+}
