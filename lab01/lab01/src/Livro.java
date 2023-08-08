1733a6e69f4347dd752a601cd17f0e5560a0b208
Canela-san <sstarksan@gmail.com>
2023-08-07 22:28:28 -0300

    lab01

--- lab01/lab01/src/Livro.java
+++ lab01/lab01/src/Livro.java
@@ -1,9 +1,58 @@
 public class Livro {
+    private int id;
     private String nome;
     private String edicao;
     private String categoria;
     private String descricao;
     private int quantidade;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEdicao() {
+        return this.edicao;
+    }
+
+    public void setEdicao(String edicao) {
+        this.edicao = edicao;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getQuantidade() {
+        return this.quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
     
 
 
---
+++ lab01/lab01/src/Pessoa.java
@@ -0,0 +1,30 @@
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private int CPF;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getCPF() {
+        return this.CPF;
+    }
+
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+
+}
